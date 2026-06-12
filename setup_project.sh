e565a463bd9ce65a9a1fad17bd58ff7dadf38425
Daniella <m.nishimwe@alustudent.com>
2026-06-12 22:20:38 +0200

    I changed the attendance_checker.py from being a directory to be a file

--- setup_project.sh
+++ setup_project.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 #this implements the Student Attendance Tracker that build a "Project Factory"
 
-read -p "enter something to keep on with the procedure:" input
+Signal_Trap(){
+      trap "cp attendance_tracker_$input attendance_tracker_"$input"_archive | rm attendance_tracker_$input" SIGINT
+}
+read -p "enter a word for procedure to keep on:" input
 
  if [ -d "attendance_tracker_$input" ]
    then
 	   echo "You can immediately access the files inside the directory"
  else
+       echo "Creating parent directories, child directories and files"
+       echo "--------------------------------------------------------"
 	 mkdir attendance_tracker_$input
-  	 mkdir attendance_tracker_$input/attendance_checker.py
+  	 touch attendance_tracker_$input/attendance_checker.py
 	 mkdir attendance_tracker_$input/Helpers
  	 mkdir attendance_tracker_$input/reports
  fi
- #creating the files insided of Helpers and reports directories
-   echo "Creating the files inside the Helpers and reports directories"
-   echo "-------------------------------------------------------------"
+ #creating the files inside the Helpers and reports directories
+
      touch attendance_tracker_$input/Helpers/assets.csv
      touch attendance_tracker_$input/Helpers/config.json
      touch attendance_tracker_$input/reports/reports.log
 
-Signal_Trap(){
-
-        trap "cp attendance_tracker_$input attendance_tracker_"$input"_archive | rm attendance_tracker_$input" SIGINT
-}
 Signal_Trap
